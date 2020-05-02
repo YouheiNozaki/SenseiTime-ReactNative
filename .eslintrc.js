ce222bcc031f402bada7cc92341a15b1ac6fe9ce
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-05-02 22:29:48 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  root: true,
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    '@react-native-community',
+    'plugin:react-native/all',
+    'plugin:jest/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'react-native', '@typescript-eslint', 'jest'],
+  rules: {
+    'no-console': 'warn',
+    yoda: 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react-native/no-raw-text': 'off',
+    'react-native/sort-styles': 'off',
+  },
+};
