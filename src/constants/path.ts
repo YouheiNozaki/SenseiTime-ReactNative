e71bbfbe7610f763e1388cf5c9e3ce057719b672
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-05-03 16:50:37 +0900

    path.tsを作成

---
+++ src/constants/path.ts
@@ -0,0 +1,20 @@
+export const INITIAL = "INITIAL";
+
+// ログイン画面
+export const CHOOSE_LOGIN = "CHOOSE_LOGIN";
+export const SIGNUP = "SIGNUP";
+export const SIGNIN = "SIGNIN";
+
+// Bottom Tab　ルーティング
+export const HOME = "HOME";
+export const ADJUSTMENTLIST = "ADJUSTMENTLIST";
+
+// Stack ルーティング
+export const INPUT = "INPUTLIST";
+export const INPUT_ADJUSTMENT = "INPUT_ADJUSTMENT";
+export const INPUT_WORKTIME = "INPUT_ WORKTIME";
+
+// Drawer ルーティング
+export const USER_INFO = "USER_INFO";
+export const CONTACT = "CONTACT";
+export const LOGOUT = "LOGOUT";
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export const Index = () => {
+  return <View style={styles.container} />;
+};
