e9cf25d74507fa91e5aabc213239b810650bd3ee
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-05-06 16:23:30 +0900

    HeaderLeftをアイコンに変更

--- src/routes/Header/HeaderLeft.tsx
+++ src/routes/Header/HeaderLeft.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback } from "react";
-import { TouchableOpacity, Text } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 import { useNavigation } from "@react-navigation/native";
 import { DrawerActions } from "@react-navigation/routers";
+import { COLOR } from "../../constants/theme";
 
 export const HeaderLeft = () => {
   const { dispatch } = useNavigation();
@@ -9,8 +10,11 @@ export const HeaderLeft = () => {
     dispatch(DrawerActions.openDrawer());
   }, [dispatch]);
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>open</Text>
-    </TouchableOpacity>
+    <Icon.Button
+      name="bars"
+      color={COLOR.WHITE}
+      backgroundColor={COLOR.WHITE}
+      onPress={onPress}
+    />
   );
 };
